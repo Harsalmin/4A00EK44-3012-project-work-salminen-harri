144ef59a97c4375a41da81926bd58213ab86b583
Harri <harkiel89@gmail.com>
2022-12-16 16:11:33 +0200

    Finished javadoc for UpdateMenu

--- src/UpdateMenu.java
+++ src/UpdateMenu.java
@@ -1,14 +1,18 @@
 import java.io.Console;
 
 /**
- * 
+ * The menu class for updating existing contacts. The update are itemized for 
+ * each variable, and validated before update goes through.
  */
 public class UpdateMenu {
     public static Console c = System.console();
     public static ContactsManager manager = ContactsApp.manager;
 
     /**
-     * 
+     * The main update menu method. Displays all contact information as short 
+     * versions, only displaying personal ID, first name and last name. Then 
+     * user is asked to pick an option, or go back to main menu. The contact 
+     * index gets taken from the option chosen, and sent forward.
      */
     public static void updateContactMenu() {
         System.out.println("Please select contact to update: ");
@@ -25,8 +29,13 @@ public class UpdateMenu {
     }
 
     /**
+     * After picking a contact to update in the updateContactMenu(), this menu 
+     * is shown, asking the user to specify which information about the 
+     * contact they want to update. The method takes the index of contact in 
+     * question from the previous menu, and displays the contact here in full.
      * 
-     * @param index
+     * @param index Index of the Contact object to be updated in the 
+     * ContactsManager object ArrayList.
      */
     public static void updateContactItemMenu(int index) {
         Contact contact = manager.getContact(index);
@@ -46,8 +55,12 @@ public class UpdateMenu {
     }
 
     /**
-     * 
-     * @param index
+     * The menu switch method after updateContactItemMenu, which asks for the 
+     * option of what user wants to update or go back to main update menu. 
+     * After choosing the option, an appropriate method is called.
+     *  
+     * @param index Index of the Contact object to be updated in the 
+     * ContactsManager object ArrayList.
      */
     public static void updateMenuSwitch(int index) {
         int option = ContactsApp.askOption(7, 1);
@@ -76,15 +89,23 @@ public class UpdateMenu {
     }
 
     /**
+     * The method for updating personal ID. The user gets some instructions on
+     * what the personal ID should look like. Then the user is prompted to
+     * enter the personal ID update of their choosing. If the update fails,
+     * an exception is thrown all the way from Contact class validations to
+     * here, and the message is displayed. Success message is also shown, along
+     * with the updated contact information. Either way, the user is sent back
+     * to the update item selection menu.
      * 
-     * @param index
+     * @param index Index of the Contact object to be updated in the
+     *              ContactsManager object ArrayList.
      */
     public static void updateId(int index) {
         System.out.println("""
                 Please use the format of 6 digits followed by +/-/A followed
                 by 3 digits and either a digit or letter from group
                 {A,B,C,D,E,F,H,J,K,L,M,N,P,R,S,T,U,V,W,X,Y}.The first 6 digits
-                are for day, month and year of birth.
+                are for day, month and year of birth, for example 300499-675T.
                 """);
         System.out.print("Please enter new personal ID: ");
         try {
@@ -99,8 +120,16 @@ public class UpdateMenu {
     }
 
     /**
+     * The method for updating first name. The user gets some instructions on
+     * what the first name should look like. Then the user is prompted to
+     * enter the first name update of their choosing. If the update fails,
+     * an exception is thrown all the way from Contact class validations to
+     * here, and the message is displayed. Success message is also shown, along
+     * with the updated contact information. Either way, the user is sent back
+     * to the update item selection menu.
      * 
-     * @param index
+     * @param index Index of the Contact object to be updated in the
+     *              ContactsManager object ArrayList.
      */
     public static void updateFirstName(int index) {
         System.out.println("""
@@ -120,8 +149,16 @@ public class UpdateMenu {
     }
 
     /**
+     * The method for updating last name. The user gets some instructions on
+     * what the last nane should look like. Then the user is prompted to
+     * enter the last name update of their choosing. If the update fails,
+     * an exception is thrown all the way from Contact class validations to
+     * here, and the message is displayed. Success message is also shown, along
+     * with the updated contact information. Either way, the user is sent back
+     * to the update item selection menu.
      * 
-     * @param index
+     * @param index Index of the Contact object to be updated in the
+     *              ContactsManager object ArrayList.
      */
     public static void updateLastName(int index) {
         System.out.println("""
@@ -141,13 +178,22 @@ public class UpdateMenu {
     }
 
     /**
+     * The method for updating phone number. The user gets some instructions on
+     * what the phone number should look like. Then the user is prompted to
+     * enter the phone number update of their choosing. If the update fails,
+     * an exception is thrown all the way from Contact class validations to
+     * here, and the message is displayed. Success message is also shown, along 
+     * with the updated contact information. Either way, the user is sent back 
+     * to the update item selection menu.
      * 
-     * @param index
+     * @param index Index of the Contact object to be updated in the
+     *              ContactsManager object ArrayList.
      */
     public static void updatePhoneNumber(int index) {
         System.out.println("""
                 Please use the format of +358 plus the phone number without
-                the first zero when entering new phone number.
+                the first zero when entering new phone number. Phone number 
+                should be at 6 digits long, for example +358406767.
                 """);
         System.out.println("Please enter new phone number: ");
         try {
@@ -163,8 +209,16 @@ public class UpdateMenu {
     }
 
     /**
+     * The method for updating address. The user gets some instructions on
+     * what the address should look like. Then the user is prompted to
+     * enter the address update of their choosing. If the update fails,
+     * an exception is thrown all the way from Contact class validations to
+     * here, and the message is displayed. Success message is also shown, along
+     * with the updated contact information. Either way, the user is sent back
+     * to the update item selection menu.
      * 
-     * @param index
+     * @param index Index of the Contact object to be updated in the
+     *              ContactsManager object ArrayList.
      */
     public static void updateAddress(int index) {
         System.out.println("""
@@ -185,8 +239,16 @@ public class UpdateMenu {
     }
 
     /**
+     * The method for updating e-mail. The user gets some instructions on
+     * what the e-mail should look like. Then the user is prompted to
+     * enter the e-mail update of their choosing. If the update fails,
+     * an exception is thrown all the way from Contact class validations to
+     * here, and the message is displayed. Success message is also shown, along
+     * with the updated contact information. Either way, the user is sent back
+     * to the update item selection menu.
      * 
-     * @param index
+     * @param index Index of the Contact object to be updated in the
+     *              ContactsManager object ArrayList.
      */
     public static void updateEmail(int index) {
         System.out.println("""
